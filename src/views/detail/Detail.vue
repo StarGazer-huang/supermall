c18ed5c87f484623a91c0330ad9f06be196f43f6
Stargazer <73180155+stargazer-huang@users.noreply.github.com>
2022-10-23 16:26:43 +0800

    resolve scroll bug

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,19 +1,38 @@
 <template>
-  <div>
-    <h2>{{iid}}</h2>
+  <div id="detail">
+    <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
   </div>
 </template>
 
 <script>
+  import DetailNavBar from "./childComponents/DetailNavBar";
+  import DetailSwiper from "./childComponents/DetailSwiper";
+
+  import {getDetail} from "../../network/detail";
+
   export default {
     name: "Detail",
+    components: {
+      DetailSwiper,
+      DetailNavBar
+    },
     data(){
       return {
-        iid: null
+        iid: null,
+        topImages: []
       }
     },
     activated() {
+      //1.保存传入的iid
       this.iid = this.$route.params.iid
+
+      //2.根据iid请求详情数据
+      getDetail(this.iid).then(res => {
+        console.log(res);
+        //1.获取顶部的图片轮播数据
+        this.topImages = res.result.itemInfo.topImages
+      })
     }
   }
 </script>
