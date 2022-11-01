4b6808b115b34c2ca222ff1a378820cd49415de8
Stargazer <73180155+stargazer-huang@users.noreply.github.com>
2022-11-01 16:57:51 +0800

    Some bugs have been fixed

--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -1,13 +1,44 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <!--导航-->
+      <div slot="center">购物车({{cartLength}})</div>
+    </nav-bar>
+    <!--商品的列表-->
+    <cart-list></cart-list>
+    <!--底部汇总-->
+  </div>
 </template>
 
 <script>
+  import NavBar from "../../components/common/navbar/NavBar";
+  import CartList from "./childComps/CartList";
+
+  import Scroll from "../../components/common/scroll/Scroll";
+
+  import {mapGetters} from "vuex"
+
   export default {
-    name: "Shopcart"
+    name: "Shopcart",
+    components: {
+      NavBar,
+      CartList,
+      Scroll
+    },
+    computed: {
+      ...mapGetters(['cartLength'])
+    }
   }
 </script>
 
 <style scoped>
+  .cart{
+    height: 100vh;
+  }
 
+  .nav-bar{
+    background-color: var(--color-tint);
+    color: #fff;
+    /*font-weight: 700;*/
+  }
 </style>
